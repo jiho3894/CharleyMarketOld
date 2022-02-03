f58aead0db9b271e9eed194a84dfc65fac3a7ffe
unknown <crsn1111@gmail.com>
2022-02-03 18:25:17 +0900

    tailwind

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,8 +6,17 @@ const Home: NextPage = () => {
       <div className="rounded-3xl bg-white p-6 shadow-xl">
         <span className="text-3xl font-semibold">Select Item</span>
         <div className="my-2 flex justify-between">
-          <span className="text-gray-500">Grey Chair</span>
-          <span className="font-semibold">$19</span>
+          <ul>
+            {[1, 2, 3, 4].map((i) => (
+              <div
+                key={i}
+                className="my-2 flex justify-between first:bg-teal-50 last:bg-amber-50 odd:bg-blue-50 even:bg-yellow-50"
+              >
+                <span className="text-gray-500">Grey Chair</span>
+                <span className="font-semibold">$19</span>
+              </div>
+            ))}
+          </ul>
         </div>
         <div className="flex justify-between">
           <span className="text-gray-500">Grey Chair</span>
@@ -29,7 +38,7 @@ const Home: NextPage = () => {
           </button>
         </div>
       </div>
-      <div className="overflow-hidden rounded-3xl bg-white shadow-xl">
+      <div className="group overflow-hidden rounded-3xl bg-white shadow-xl">
         <div className="bg-blue-500 p-6 pb-14">
           <span className="text-2xl text-white">Profile</span>
         </div>
@@ -39,7 +48,7 @@ const Home: NextPage = () => {
               <span className="text-sm text-gray-500">Orders</span>
               <span className="font-medium">340</span>
             </div>
-            <div className="h-24 w-24 rounded-full bg-red-400" />
+            <div className="h-24 w-24 rounded-full bg-red-400 transition-colors group-hover:bg-red-300" />
             <div className="flex flex-col items-center">
               <span className="text-sm text-gray-500">Orders</span>
               <span className="font-medium">340</span>
@@ -95,7 +104,37 @@ const Home: NextPage = () => {
           </div>
         </div>
       </div>
-      <div className="rounded-2xl bg-white p-10 shadow-xl"></div>
+      <div className="rounded-2xl bg-white p-10 shadow-xl">
+        <form className="flex flex-col space-y-3">
+          <input
+            className="peer rounded-xl border border-gray-400 p-1 valid:border-blue-400 invalid:border-red-400"
+            type="text"
+            required
+            placeholder="ID"
+          />
+
+          <span className="hidden peer-invalid:block ">ID 입력</span>
+          <span className="hidden peer-valid:block ">마자요</span>
+          <input type="submit" value="login" />
+        </form>
+      </div>
+      <div className="rounded-2xl bg-white p-10 shadow-xl">
+        <input
+          type="file"
+          className="file:border file:bg-red-400 file:hover:cursor-pointer file:hover:bg-red-600"
+        />
+        <p className="first-letter:text-3xl first-letter:hover:text-blue-500">hello</p>
+        <details className="">
+          <summary className="cursor-pointer select-none">
+            What are you doing?
+          </summary>
+          <span className="">i dont no</span>
+          <ul className="list-disc marker:text-blue-400">
+            <li>1</li>
+            <li>2</li>
+          </ul>
+        </details>
+      </div>
     </div>
   );
 };
